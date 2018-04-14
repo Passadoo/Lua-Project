d023dc5d113776e875814b21b415f0af8b9b080e
Urnark <christoffer5511@gmail.com>
2018-04-14 15:22:15 +0200

    Added a basic window

--- Scripting Project/Main.cpp
+++ Scripting Project/Main.cpp
@@ -1,5 +1,45 @@
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(800, 600), "Scriptning Project!", sf::Style::Close | sf::Style::Resize);
+
+	sf::Clock clock;
+
+	sf::Time time;
+	sf::Event event;
+	while (window.isOpen())
+	{
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Escape)
+				{
+					window.close();
+				}
+			}
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::GainedFocus:
+				break;
+			case sf::Event::LostFocus:
+				break;
+			}
+		}
+
+		sf::CircleShape shape(50);
+		shape.setFillColor(sf::Color(100, 250, 50));
+
+		window.clear(sf::Color(0, 0, 0, 255));
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
