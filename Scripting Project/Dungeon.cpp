945416953a4c9948b5cef85be71d8664b36d625b
Salomon Ljungberg Fayyazuddin <salmanlf@gmail.com>
2018-04-24 21:48:55 +0200

    Started implementing reading of RoomInfo.txt in Dungeon.cpp

--- Scripting Project/Dungeon.cpp
+++ Scripting Project/Dungeon.cpp
@@ -9,6 +9,35 @@ Dungeon::Dungeon()
 
 Dungeon::Dungeon(int pNrOfRooms)
 {
+	ifstream ifs;
+	char input;
+
+	ifs.open("Resources/Rooms/RoomInfo.txt");
+
+	if (ifs.fail())
+	{
+		std::cout << "Could not open RoomInfo.txt" << std:: endl;
+	}
+
+	vector<string> lines;
+	vector<string> filepaths;
+	vector<string> doorLocations;
+	string word;
+
+	while (!ifs.eof())
+	{
+		ifs >> word; //read filepath
+		filepaths.push_back(word);
+		ifs >> word; //read door locations in form of 4 ints
+		doorLocations.push_back(word);
+	}
+
+	if (filepaths.size() > 0)
+	{
+		mArrayLength = filepaths.size();
+	}
+
+
 	mArrayLength = (2 * pNrOfRooms) + 1;
 
 	mRooms = new Room*[mArrayLength];
